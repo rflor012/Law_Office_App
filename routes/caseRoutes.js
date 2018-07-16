b551938accec1c3bd94850ce59c290f0d5d237e8
Rodrigo Flores <rflor012@gmail.com>
2018-07-15 20:29:47 -0400

    Issues with loging error messages.

--- routes/caseRoutes.js
+++ routes/caseRoutes.js
@@ -3,5 +3,15 @@ const caseRouter= express.Router();
 const User      = require('../models/user');
 const bcrypt    = require('bcryptjs');
 const passport  = require('passport');
+const ensureLogin = require('connect-ensure-login');
+
+caseRouter.get('/create/client', ensureLogin.ensureLoggedIn(), (req, res, next)=>{
+  if(!req.session.currentUser){
+    res.redirect('/login');
+    return;
+  }
+  res.render('userViews/createClient');
+});
+
 
 module.exports = caseRouter;
