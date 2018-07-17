99f3a86e3954b62294797523e117a5cecd046166
Rodrigo Flores <rflor012@gmail.com>
2018-07-17 16:21:51 -0400

    Made small changes, spoke to manny, need to consolidate payment models into a key:value pair, will make a model for CONTRACTS only, payments will be encompassed into case model

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -6,11 +6,23 @@ const Payment   = require('../models/payment');
 const bcrypt    = require('bcryptjs');
 const passport  = require('passport');
 
-paymentRouter.get('/payments/new', (req, res, next)=>{
+paymentRouter.get('/payments/all', (req, res, next)=>{
   res.render("paymentViews/paymentsIndex");
 });
 
+
 paymentRouter.get('/payments/:id/create', (req, res, next)=>{
+  Case.findById(req.params.id)
+  .then((theCase)=>{
+
+    res.render('paymentViews/createPayment');
+  })
+  .catch((err)=>{
+    next(err);
+  });
+});
+
+paymentRouter.get('/payments/:id/create/contract', (req, res, next)=>{
   Case.findById(req.params.id)
   .then((theCase)=>{
 
